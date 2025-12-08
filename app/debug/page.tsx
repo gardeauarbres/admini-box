4cd859489430992044166d659cd81b9a58b4ac62
gardeauarbres <193312403+gardeauarbres@users.noreply.github.com>
2025-12-08 21:52:20 +0100

    Cleanup: Remove debug files after Vercel diagnostic

--- app/debug/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DebugPage() {
-    return (
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-            <h1>Debug Route Works!</h1>
-            <p>App Router is functioning correctly.</p>
-        </div>
-    );
-}
